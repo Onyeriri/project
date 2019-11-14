ef2eac0855c843cd65882d56fb82320c1e3aea7e
Onyeriri <viktorchinonso@gmail.com>
2019-11-14 16:23:38 +0100

    bug(database): fix travis issue

--- src/test/testDatabase.js
+++ src/test/testDatabase.js
@@ -1,7 +1,8 @@
 import DB from '../DB';
 
+const dropEmpTable = 'DROP TABLE IF EXISTS employees CASCADE';
 const employeTable = `CREATE TABLE IF NOT EXISTS employees(
-employeeid bigserial NOT NULL,
+userid bigserial NOT NULL,
 email character varying NOT NULL,
 password character varying NOT NULL,
 address character varying,
@@ -10,47 +11,51 @@ jobrole character varying NOT NULL,
 firstname character varying(100),
 lastname character varying(100),
 department character varying(100),
-CONSTRAINT employee_pkey PRIMARY KEY(employeeid)
+CONSTRAINT employee_pkey PRIMARY KEY(userid)
 )`;
 
+const dropArticleTable = 'DROP TABLE IF EXISTS articles CASCADE';
 const articleTable = `CREATE TABLE IF NOT EXISTS articles(
 articleid bigserial NOT NULL,
-employeeid bigserial,
+authorid bigserial,
 title character varying NOT NULL,
 article character varying NOT NULL,
 CONSTRAINT article_pkey PRIMARY KEY(articleid),
-CONSTRAINT article_id_employee_idfkey FOREIGN KEY(employeeid)
+CONSTRAINT article_id_employee_idfkey FOREIGN KEY(authorid)
 REFERENCES employees
 )`;
 
+const dropgifsTable = 'DROP TABLE IF EXISTS gifs CASCADE';
 const gifTable = `CREATE TABLE IF NOT EXISTS gifs(
 gifid bigserial NOT NULL,
-employeeid bigserial,
+authorid bigserial,
 title character varying NOT NULL,
-image_url character varying NOT NULL,
+imageurl character varying NOT NULL,
 CONSTRAINT gif_pkey PRIMARY KEY(gifid),
-CONSTRAINT gif_id_employee_idfkey FOREIGN KEY(employeeid)
+CONSTRAINT gif_id_employee_idfkey FOREIGN KEY(authorid)
 REFERENCES employees
 )`;
 
+const dropArticleCommentTable =  'DROP TABLE IF EXISTS comments_articles CASCADE';
 const commentArticleTable = `CREATE TABLE IF NOT EXISTS comments_articles(
 commentid bigserial NOT NULL,
-employeeid bigserial,
+authorid bigserial,
 articleid bigserial,
 comment character varying NOT NULL,
 CONSTRAINT comment_article_pkey PRIMARY KEY(commentid),
-CONSTRAINT article_comment_id_employee_idfkey FOREIGN KEY(employeeid)REFERENCES employees,
+CONSTRAINT article_comment_id_employee_idfkey FOREIGN KEY(authorid)REFERENCES employees,
 CONSTRAINT article_comment_id_article_idfkey FOREIGN KEY(articleid)
 REFERENCES articles
 )`;
 
+const dropGifCommentTable = 'DROP TABLE IF EXISTS comments_gifs CASCADE';
 const gifCommentTable = `CREATE TABLE IF NOT EXISTS comments_gifs(
 commentid bigserial NOT NULL,
-employeeid bigserial,
+authorid bigserial,
 gifid bigserial,
 comment character varying NOT NULL,
 CONSTRAINT comment_gif_pkey PRIMARY KEY(commentid),
-CONSTRAINT gif_comment_id_employee_idfkey FOREIGN KEY(employeeid)
+CONSTRAINT gif_comment_id_employee_idfkey FOREIGN KEY(authorid)
 REFERENCES employees,
 CONSTRAINT gif_comment_id_article_idfkey FOREIGN KEY(gifid)
 REFERENCES gifs
@@ -58,6 +63,12 @@ REFERENCES gifs
 
 class Table {
   static async createTables() {
+    await DB.query(dropGifCommentTable);
+    await DB.query(dropArticleCommentTable);
+    await DB.query(dropgifsTable);
+    await DB.query(dropArticleTable);
+    await DB.query(dropEmpTable);
+
     await DB.query(employeTable);
     await DB.query(gifTable);
     await DB.query(articleTable);
